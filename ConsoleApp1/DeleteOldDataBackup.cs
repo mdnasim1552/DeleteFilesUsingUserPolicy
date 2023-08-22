bede9d261a2cca5279e2805f7ed11417f493cedd
Md. Nasim Uddin <md.nasim1552@gmail.com>
2023-08-22 06:29:25 +0600

    22-August-2023

---
+++ ConsoleApp1/DeleteOldDataBackup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class DeleteOldDataBackup
+    {
+        public static void DeleteOldBackups(string backupFolderPath, DateTime currentDate, int keepLastDays)
+        {
+            DirectoryInfo backupDirectory = new DirectoryInfo(backupFolderPath);
+            FileInfo[] backupFiles = backupDirectory.GetFiles("*.*");
+
+            var groupedBackups = backupFiles.GroupBy(file => new { file.LastWriteTime.Year, file.LastWriteTime.Month });
+
+            foreach (var group in groupedBackups)
+            {
+                FileInfo lastBackup = group.OrderByDescending(file => file.LastWriteTime).First();
+
+                foreach (FileInfo backupFile in group)
+                {
+                    if (backupFile != lastBackup)
+                    {
+                        DateTime backupDate = backupFile.LastWriteTime;
+
+                        if (backupDate.Month == currentDate.Month)
+                        {
+                            int daysDifference = (currentDate - backupDate).Days;
+
+                            if (daysDifference > keepLastDays)
+                            {
+                                backupFile.Delete();
+                                Console.WriteLine($"Deleted: {backupFile.Name}");
+                            }
+                        }
+                        else if (backupDate < lastBackup.LastWriteTime)
+                        {
+                            // Delete backups older than the last backup of the month
+                            backupFile.Delete();
+                            Console.WriteLine($"Deleted: {backupFile.Name}");
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //string backupFolderPath = @"E:\Session_with_tanvir_vai";
+        string backupFolderPath = args[0];
+        DateTime currentDate = DateTime.Now;
+        int keepLastDays = int.Parse(args[1]);
+        //int keepLastDays = 10;
+
+        DeleteOldDataBackup.DeleteOldBackups(backupFolderPath, currentDate, keepLastDays);
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+
+    }
+
+}
